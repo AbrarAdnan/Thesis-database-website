7707ea922f73d9f107e13eb64e5ff783e0853d19
AbrarAdnan <52294804+AbrarAdnan@users.noreply.github.com>
2021-04-17 22:11:51 +0600

    initial commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!--footer-->
+	<footer>
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+					</div>
+				</div>
+				<div class="w3layouts-agile-copyrightbottom">
+					<p>Made by	<a href="https://github.com/AbrarAdnan">Abrar Adnan</a>
+					</p>
+
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
